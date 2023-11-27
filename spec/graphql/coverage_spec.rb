51ab02182de695ec3b739b08ef82e4c936c826a3
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-11-27 22:54:55 +0900

    bunlde gem

---
+++ spec/graphql/coverage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Graphql::Coverage do
+  it "has a version number" do
+    expect(Graphql::Coverage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
