7085196d5d2c6244232f953a893a6ad9ecd2498d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-11-28 22:02:14 +0900

    Add type signature

--- lib/graphql/coverage/call.rb
+++ lib/graphql/coverage/call.rb
@@ -2,8 +2,11 @@
 
 module GraphQL
   module Coverage
+    # rubocop:disable Naming/ConstantName
     # @api private
-    Call = Struct.new(:owner, :field, :result_type, keyword_init: true) do
+    Call = _ = Struct.new(:owner, :field, :result_type, keyword_init: true)
+    # rubocop:enable Naming/ConstantName
+    class Call
       def self.from_graphql_object(field:, result_type:)
         new(owner: field.owner.graphql_name, field: field.graphql_name, result_type: result_type)
       end
