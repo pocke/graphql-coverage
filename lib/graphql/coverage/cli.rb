b3bc427877aa46c10d4779943a09e10ae1703dc0
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-12-01 00:59:16 +0900

    Add CLI

---
+++ lib/graphql/coverage/cli.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module GraphQL
+  module Coverage
+    class CLI
+      def initialize(stdout:, stderr:)
+        @stdout = stdout
+        @stderr = stderr
+      end
+
+      def run(argv)
+        # @type var params: params
+        params = {
+          fail_on_uncovered: true,
+        }
+
+        args = OptionParser.new do |opts|
+          opts.banner = 'Usage: graphql-coverage [options] [result file paths]'
+          opts.on('-r', '--require PATH', 'Require a file.') do |path|
+            require path
+          end
+
+          opts.on('--[no-]fail-on-uncovered', 'Fail when there are uncovered fields (default: true).')
+        end.parse(argv)
+
+        Coverage.load(*args)
+
+        ok = Coverage.report(output: stdout)
+        if ok || !params[:fail_on_uncovered]
+          return 0
+        else
+          1
+        end
+      end
+
+      private
+
+      attr_reader :stdout, :stderr
+    end
+  end
+end
